59549975657551c6137d8a7e5c1c1eb45127a834
DoTMInDs <esmondabban3@gmail.com>
2025-05-04 20:16:09 +0000

    updated

--- hotelapp/hotelapp/settings.py
+++ hotelapp/hotelapp/settings.py
@@ -171,7 +171,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'hotelapp/staticfiles')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
