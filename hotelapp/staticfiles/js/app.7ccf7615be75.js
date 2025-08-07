2336c9b62dc0c412b307e62ff61221cea97fe075
esmond <dhonniemccony3@gmail.com>
2025-08-06 23:59:39 -1200

    refactor code

---
+++ hotelapp/staticfiles/js/app.7ccf7615be75.js
@@ -0,0 +1,19 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    console.log('clicked');
+    mobileMenu.classList.toggle('change');
+    navLinks.classList.toggle('showing');
+});
+
+const viewPrices = document.querySelectorAll('.view_price');
+const priceTag = document.getElementById('priceTag');
+
+for (const viewPrice of viewPrices) {
+    viewPrice.addEventListener('click', (e) =>{
+        let priceTag = document.getElementById('priceTag-' +e.target.dataset.pk);
+        priceTag.classList.toggle('active-price-tag')
+    })
+}
+//------------- LOGIN-&-SIGN-UP-section----------------------//
